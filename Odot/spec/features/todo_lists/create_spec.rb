f8ae9fb46ac497684cd401043a2449f2908af99c
Alan Bishop <djab1990@gmail.com>
2014-07-16 20:03:24 -0700

    More tests, validation for description

--- Odot/spec/features/todo_lists/create_spec.rb
+++ Odot/spec/features/todo_lists/create_spec.rb
@@ -30,4 +30,42 @@ describe "Creating todo_lists" do
         visit "/todo_lists"
         expect(page).to_not have_content("What I'm doing today") 
     end
+    
+    it "displays an error when a todo_list has a title less than 3 characters" do
+        expect(TodoList.count).to eq(0)
+        
+        visit "/todo_lists"
+        click_link "New Todo list"
+        expect(page).to have_content("New todo_list")
+
+        fill_in "Title", with: "Hi"
+        fill_in "Description", with: "What I'm doing today"
+        click_button "Create Todo list"
+
+        expect(page).to have_content("error")
+        expect(TodoList.count).to eq(0)
+        
+        visit "/todo_lists"
+        expect(page).to_not have_content("What I'm doing today") 
+    end
+
+    it "displays an error when a todo_list has a description less than 5 characters" do
+        expect(TodoList.count).to eq(0)
+        
+        visit "/todo_lists"
+        click_link "New Todo list"
+        expect(page).to have_content("New todo_list")
+
+        fill_in "Title", with: "Hello"
+        fill_in "Description", with: "What"
+        click_button "Create Todo list"
+
+        expect(page).to have_content("error")
+        expect(TodoList.count).to eq(0)
+        
+        visit "/todo_lists"
+        expect(page).to_not have_content("What I'm doing today") 
+    end
+
 end
+
